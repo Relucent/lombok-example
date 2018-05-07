61c427b926edfdb6804e73e3f38a563aae03ab8e
relucent <relucent@163.com>
2018-05-07 10:17:32 +0800

    @

--- src/main/java/yyl/lombok/AccessorsExample.java
+++ src/main/java/yyl/lombok/AccessorsExample.java
@@ -2,13 +2,11 @@ package yyl.lombok;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import lombok.experimental.Accessors;
 
 @Getter
 @Setter
 @Accessors(chain = true)
-@ToString
 public class AccessorsExample {
 
 	private String name;
